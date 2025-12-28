bb7ed94ed209503546276731fb5e5ac0f2d12ed9
Aroub  Al Rizq <127698586+AroubAlRizq@users.noreply.github.com>
2025-12-28 12:03:03 +0300

    Create RectangleTest.java

---
+++ OOP/Rectangle/RectangleTest.java
@@ -0,0 +1,14 @@
+
+
+public class RectangleTest {
+    
+    public static void main(String[] args){
+
+        Rectangle r1 = new Rectangle(4,40);
+        Rectangle r2 = new Rectangle(3.5, 35.9);
+        System.out.println(r1.toString());
+        System.out.println(r2.toString());
+        
+        
+    }
+}
